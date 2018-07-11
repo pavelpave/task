c32fe2c1f4ca06b07416608deee1c11c40521584
pavelpave <40344085+pavelpave@users.noreply.github.com>
2018-07-11 13:15:51 +0300

    Add files via upload

---
+++ ДЛЯ РАБОТЫ/javaScript.js
@@ -0,0 +1,8 @@
+$('.cont__click-opt').click(function () {
+		$(".cont__opis").css("display","block"); 
+		$(".cont__tex").css("display","none")
+	});
+$('.cont__click-tex').click(function () {
+    $(".cont__opis").css("display","none");
+    $(".cont__tex").css("display","block")
+})
